ab2328fa3f6887a0772cea76874c99130bb57dfd
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-07-09 23:36:34 +0545

    something

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '2.2.0'
 gem 'rails', '4.2.1'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -12,3 +13,4 @@ gem 'spree_gateway', github: 'spree/spree_gateway', branch: '3-0-stable'
 gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '3-0-stable'
 gem 'rails_12factor', group: :production
 gem 'pg'
+gem 'aws-sdk'
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,39 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  attachment_config = {
+
+  s3_credentials: {
+    access_key_id:     "AKIAIQ3RDJPY6VUWQ7DA",
+    secret_access_key: "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",
+    bucket:            "nepalihandicraftscollection"
+  },
+
+  storage:        :s3,
+  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+  s3_protocol:    "https",
+  bucket:         ENV['S3_BUCKET_NAME'],
+  url:            ":s3_domain_url",
+
+  styles: {
+    mini:     "48x48>",
+    small:    "100x100>",
+    product:  "240x240>",
+    large:    "600x600>"
+  },
+
+  path:           "/:class/:id/:style/:basename.:extension",
+  default_url:    "/:class/:id/:style/:basename.:extension",
+  default_style:  "product"
+  }
+
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
+
 end
 
 Spree.user_class = "Spree::User"
+Paperclip.interpolates(:s3_domain_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
